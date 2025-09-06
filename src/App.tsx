848b6f9fd167d877c3cec2001f792b7837f3810c
Jesus <jjcorrales76@gmail.com>
2025-09-06 16:16:40 -0600

    added types

--- src/App.tsx
+++ src/App.tsx
@@ -7,33 +7,32 @@ import Confetti from "react-confetti"
 
 export default function AssemblyEndgame() {
     // State values
-    const [currentWord, setCurrentWord] = useState<string>(() => getRandomWord())
-    const [guessedLetters, setGuessedLetters] = useState([])
+    const [currentWord, setCurrentWord] = useState<string>((): string => getRandomWord())
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
 
     // Derived values
-    const numGuessesLeft = languages.length - 1
-    const wrongGuessCount =
-        guessedLetters.filter(letter => !currentWord.includes(letter)).length
-    const isGameWon =
-        currentWord.split("").every(letter => guessedLetters.includes(letter))
-    const isGameLost = wrongGuessCount >= numGuessesLeft
-    const isGameOver = isGameWon || isGameLost
-    const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
-    const isLastGuessIncorrect = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+    const numGuessesLeft:number = languages.length - 1
+    const wrongGuessCount:number = guessedLetters.filter((letter:string):boolean => !currentWord.includes(letter)).length
+    const isGameWon:boolean = currentWord.split("").every((letter:string):boolean => guessedLetters.includes(letter))
+    const isGameLost:boolean = wrongGuessCount >= numGuessesLeft
+    const isGameOver:boolean = isGameWon || isGameLost
+    const lastGuessedLetter:string = guessedLetters[guessedLetters.length - 1]
+    const isLastGuessIncorrect:boolean = lastGuessedLetter && !currentWord.includes(lastGuessedLetter)
+
 
     // Static values
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-    function addGuessedLetter(letter) {
-        setGuessedLetters(prevLetters =>
+    function addGuessedLetter(letter:string): void {
+        setGuessedLetters((prevLetters: string[]):string[] =>
             prevLetters.includes(letter) ?
                 prevLetters :
                 [...prevLetters, letter]
         )
     }
 
-    function startNewGame() {
+    function startNewGame(): void {
         setCurrentWord(getRandomWord())
         setGuessedLetters([])
     }
